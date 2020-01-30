cdec496f88ec772520712cb1cfba11321d52ae52
Thomas McAndrew <tommcandrew@hotmail.com>
2020-01-30 02:39:08 +0000

    make createMonthArray function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import createMonthArray from "./util/createMonthArray";
 
 const App = () => {
   return <div></div>;
---
+++ src/util/createMonthArray.js
@@ -0,0 +1,38 @@
+const createMonthArray = (currentYear, month) => {
+  const monthArray = [];
+  const startOfMonth = new Date(currentYear, month);
+  let firstDayOfMonth = startOfMonth.getDay();
+  //if first day is Sunday, count as 7 instead of 0 to get 6 blanks before it
+  if (firstDayOfMonth === 0) {
+    firstDayOfMonth = 7;
+  }
+  //generate blanks for start of month
+  for (let i = 0; i < firstDayOfMonth - 1; i++) {
+    //giving blanks value of -1 to avoid CSS issues later (so content size in all cells is equal)
+    monthArray.push({ date: -1, month: month, year: currentYear });
+  }
+  //passing in 0 gives you last day of previous month
+  const endOfMonth = new Date(currentYear, month + 1, 0);
+  const lastDayOfMonth = endOfMonth.getDate();
+
+  for (let i = 0; i < lastDayOfMonth; i++) {
+    //JS dates start from 1, not 0
+    monthArray.push({
+      date: i + 1,
+      month: month,
+      year: currentYear
+    });
+  }
+
+  //generate blanks for end of month
+  let blanksRequired = 42 - monthArray.length;
+  for (let i = 0; i < blanksRequired; i++) {
+    monthArray.push({ date: -1, month: month, year: currentYear });
+  }
+
+  return monthArray;
+};
+
+console.log(createMonthArray(2019, 8));
+
+export default createMonthArray;
